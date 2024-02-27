af223244a34ddaba2569a9abad2b89a1ce87550e
Mykhailo Holub <mishataube2005@gmail.com>
2024-02-27 09:40:24 +0200

    Update formating in __main__.py

--- src/PythonTableConsole/__main__.py
+++ src/PythonTableConsole/__main__.py
@@ -1,19 +1,22 @@
-class PythonTableConsole: #main class for creating and handling PythonTableConsole
-    def __init__(self, contains = []): #creates new table with specified content
-        self.contains = contains #contains must consist of [[Column0Row0,..., Column0RowN], [Column1Row0,..., Column1RowM],...,[ColumnKRow0,..., ColumnKRowS]]
-    def __update(self): #internal function for handling various problems
+# __main__.py
+
+class PythonTableConsole:  # main class for creating and handling PythonTableConsole
+    def __init__(self, contains=[]):  # creates new table with specified content
+        self.contains = contains  # contains must consist of [[Column0Row0,..., Column0RowN], [Column1Row0,..., Column1RowM],...,[ColumnKRow0,..., ColumnKRowS]]
+
+    def __update(self):  # internal function for handling various problems
         max_height = 0
         i = 0
-        while i < len(self.contains): #max_height finder
+        while i < len(self.contains):  # max_height finder
             current_height = 0
             if self.contains[i]:
                 current_height = len(self.contains[i])
-            
+
             if max_height < current_height:
                 max_height = current_height
-            i+=1
+            i += 1
         i = 0
-        while i < len(self.contains): #adds empty rows to columns that are shorter than longest column
+        while i < len(self.contains):  # adds empty rows to columns that are shorter than longest column
             current_height = 0
             if self.contains[i]:
                 current_height = len(self.contains[i])
@@ -21,16 +24,19 @@ class PythonTableConsole: #main class for creating and handling PythonTableConso
                 ii = current_height
                 while ii < max_height:
                     self.contains[i].append("")
-                    ii+=1
-            i+=1
-    def width(self): #returns the number of columns in the table
+                    ii += 1
+            i += 1
+
+    def width(self):  # returns the number of columns in the table
         return len(self.contains)
-    def height(self): #return the number of rows in the table
+
+    def height(self):  # return the number of rows in the table
         self.__update()
         if self.contains:
             return len(self.contains[0])
         else:
             return 0
+
     def __str__(self):
         self.__update()
         out_text = ""
@@ -42,19 +48,20 @@ class PythonTableConsole: #main class for creating and handling PythonTableConso
             while ii < self.height():
                 if column_max_width[i] < len(str(self.contains[i][ii])):
                     column_max_width[i] = len(str(self.contains[i][ii]))
-                ii+=1
-            i+=1
+                ii += 1
+            i += 1
         i = 0
         while i < self.height():
             ii = 0
             while ii < self.width():
-                out_text += "|" + " "*(column_max_width[ii] - len(str(self.contains[ii][i]))) + str(self.contains[ii][i])
-                ii+=1
-            out_text+="|\n"
-            i+=1
+                out_text += "|" + " " * (column_max_width[ii] - len(str(self.contains[ii][i]))) + str(
+                    self.contains[ii][i])
+                ii += 1
+            out_text += "|\n"
+            i += 1
         return out_text
 
-    def transpose(self): #transposes the table (rows to columns,columns to rows)
+    def transpose(self):  # transposes the table (rows to columns,columns to rows)
         old_self = self.contains
         new_self = []
         for i in range(self.height()):
@@ -64,11 +71,12 @@ class PythonTableConsole: #main class for creating and handling PythonTableConso
         self.contains = new_self
         self.__update()
 
-    def sort_by_column(self, column_index, skip_n_rows=0): #sorts table by specified column. To sort by row: transpose, sort_by_column, transpose.
+    def sort_by_column(self, column_index,
+                       skip_n_rows=0):  # sorts table by specified column. To sort by row: transpose, sort_by_column, transpose.
         self.transpose()
-        for i in range(skip_n_rows,self.width()):
-            for ii in range(skip_n_rows,self.width()-1):
-                if self.contains[ii][column_index] < self.contains[ii+1][column_index]:
-                    self.contains[ii], self.contains[ii+1] = self.contains[ii+1], self.contains[ii]
+        for i in range(skip_n_rows, self.width()):
+            for ii in range(skip_n_rows, self.width() - 1):
+                if self.contains[ii][column_index] < self.contains[ii + 1][column_index]:
+                    self.contains[ii], self.contains[ii + 1] = self.contains[ii + 1], self.contains[ii]
         self.transpose()
         self.__update()
